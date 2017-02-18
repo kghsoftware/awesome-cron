95b867fb787aa376266bf5f997accbe1f1acb19d
Anh Phung <anh@kghsoftware.com>
2017-02-19 01:39:14 +0700

    update cron

--- cron_page_1m.php
+++ cron_page_1m.php
@@ -1,3 +1,8 @@
-<?php
-	echo "1";
+<?php 
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_URL, http://timkiemfb.com/cronjob-page-one/?limit=500); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	$output = curl_exec($ch); 
+	curl_close($ch);      
+	die($output);
 ?>
