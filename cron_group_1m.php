5121b914b1d4a0f80c688736b1f11772ba5a6579
Anh Phung <anh@kghsoftware.com>
2017-02-19 03:42:36 +0700

    update group scan

---
+++ cron_group_1m.php
@@ -0,0 +1,8 @@
+<?php 
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_URL, "http://timkiemfb.com/cronjob-group-one/?limit=500"); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	$output = curl_exec($ch); 
+	curl_close($ch);      
+	die($output);
+?>
